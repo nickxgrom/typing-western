ff7727f344a2a3d821a185bc62c761ac15778ba7
Nick Gromyko <nxgr.dev@gmail.com>
2023-07-05 19:30:54 +0600

    initialize

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+	  indent: ["error", "tab"],
+	  quotes: ["error", "double"],
+	  semi: ["error", "never"],
+      "jsx-quotes": ["error", "prefer-double"],
+	  "react/jsx-max-props-per-line": 1,
+	  "linebreak-style": ["error", "windows"],
+	  "react/react-in-jsx-scope": "off",
+	  "no-const-assign": "error",
+	  "no-constant-condition": "error",
+	  "no-trailing-spaces": "error",
+  },
+}
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+	return (
+		<h1 className="app">Typing Western</h1>
+	)
+}
+
+export default App
