f14b5283317d66d67ccc8e592385bc6dd43b90ea
Nick Gromyko <nxgr.dev@gmail.com>
2023-07-09 22:53:27 +0600

    add(layout)
    router
    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/header/Header.jsx"
+import router from "./router.jsx"
+import {RouterProvider} from "react-router-dom"
+
 
 function App() {
     return (
-        <div className="container">
-            <Header></Header>
+        <div className="container py-8">
+            <Header />
+            <div className="pt-8">
+                <RouterProvider router={router} />
+            </div>
         </div>
     )
 }
